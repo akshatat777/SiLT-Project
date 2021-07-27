f882012bf1aa19247330d8df934fab33105dab92
kw-0 <46660348+kw-0@users.noreply.github.com>
2021-07-27 16:03:40 -0400

    adds comments and removes the bounding box from the final hand image

--- hand_cropping.py
+++ hand_cropping.py
@@ -12,10 +12,12 @@ hands = mpHands.Hands(static_image_mode=False,
                       min_tracking_confidence=0.45)
 mpDraw = mp.solutions.drawing_utils
 
-margin = 40
+margin = 40  # margin gives some space between the tips of fingers and the bounding box (bbox) measured in pixels
+thickness_of_bbox = 2
 
 
 while True:
+    # plays recording from camera and processes each image
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
@@ -28,6 +30,8 @@ while True:
             for lm in handLms.landmark:
                 h, w, c = img.shape
 
+                # here we multiply by the width and height because the landmarks are auto-normalized based on
+                # the width and height of the displayed image
                 landmark_listx.append((lm.x*w))
                 landmark_listy.append((lm.y*h))
                 
@@ -43,17 +47,21 @@ while True:
                 mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
                 """
 
+            # creating the starts and ends of the box around the hand (+ some margin)
             start = (int(max(landmark_listx))+margin, int(max(landmark_listy))+margin)
             end = (int(min(landmark_listx))-margin, int(min(landmark_listy))-margin)
 
-            img = cv2.rectangle(img, start, end, color=(255, 166, 48), thickness=2)
+            img = cv2.rectangle(img, start, end, color=(255, 166, 48), thickness=thickness_of_bbox)
 
-    cv2.imshow("Image", img)
+    cv2.imshow("image", img)
 
 
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
-cv2.imwrite('image.jpg', img[:][end[1]:start[1], end[0]:start[0]])
+# saves the image as a jpg and crops it down to just the hand (the +thickness+1 is to remove the box and the gradient area between the box and actual hand)
+# the [:] at the beginning is to apply this crop to all color channels
+cv2.imwrite('image.jpg', img[:][end[1]+(thickness_of_bbox+1):start[1]-(thickness_of_bbox-1),
+                                end[0]+(thickness_of_bbox+1):start[0]-(thickness_of_bbox-1)])
 
 cv2.destroyAllWindows()
